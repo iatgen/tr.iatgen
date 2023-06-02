6cb6b392abe92e5196bf7dc772c7774390c8b196
Joao-O-Santos <joao.filipe.santos@campus.ul.pt>
2023-06-02 19:37:43 +0100

    Add export.template unit tests

--- tests/testthat/test-export_template.R
+++ tests/testthat/test-export_template.R
@@ -1,3 +1,17 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("export.template() returns a single value", {
+  out_len <- length(export.template())
+
+  expect_equal(out_len, 1)
+})
+
+test_that("export.template() returns a value of class character", {
+  out_class <- class(export.template())
+
+  expect_equal(out_class, "character")
+})
+
+test_that("export.template() returns a path ending in templates/en_en.csv", {
+  out_ending <- grepl("templates/en_en.csv$", export.template())
+
+  expect_equal(out_ending, TRUE)
 })
